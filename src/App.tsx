60a0a1b0afaa3bdcdd64c85be838bfed4b568dca
Huyen Tran <ttminhuyen1111@gmail.com>
2024-01-16 17:41:49 +0700

    handle change .ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { lazy, Suspense, useContext, useEffect, useState } from 'react';
-import { Route,Routes } from 'react-router-dom';
-import NotFound from './RouterDom/pages/NotFound/NotFound';
-import Loading from './loading';
-import Footer from './component/Footer/Footer';
-import AllPage from './RouterDom/Page'
-import Login from './RouterDom/pages/login/Login';
-import Header from './component/Header/Header';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { lazy, Suspense, useContext, useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./RouterDom/pages/NotFound/NotFound";
+import Loading from "./Loading";
+import Footer from "./component/Footer/Footer";
+import AllPage from "./RouterDom/Page";
+import Login from "./RouterDom/pages/login/Login";
+import Header from "./component/Header/Header";
 function App() {
-  const showPageUser=(AllPage:any)=>{
-    if(AllPage.length>0){
-      return AllPage.map((page:any,index:any)=>(
-          <>
-           <Route
-          key={index}
-          path={page.path}
-          element={<Suspense fallback={<Loading/>}>
-          {page.main}
-        </Suspense>
-        }
-        />
-          </>
-      ))
-  }}
+  const showPageUser = (AllPage: any) => {
+    if (AllPage.length > 0) {
+      return AllPage.map((page: any, index: any) => (
+        <>
+          <Route
+            key={index}
+            path={page.path}
+            element={<Suspense fallback={<Loading />}>{page.main}</Suspense>}
+          />
+        </>
+      ));
+    }
+  };
 
   return (
     <div className="app">
       {/* <Header/> */}
-        <Routes>
-         { showPageUser(AllPage)}
-         <Route
-          path='/*'
-          element={<NotFound/>}
-         />
+      <Routes>
+        {showPageUser(AllPage)}
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       {/* <Footer/> */}
     </div>
--- src/RouterDom/pages/Home/Home.tsx
+++ src/RouterDom/pages/Home/Home.tsx
@@ -1,50 +1,31 @@
-import React, { useEffect } from 'react'
-import "./index.css"
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllUser } from '../../../feature/user/pathApi';
-import { AreaCenter } from './araCenter/AreaCenter';
+import React, { useEffect } from "react";
+import "./index.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllUser } from "../../../feature/user/pathApi";
+import { AreaCenter } from "./araCenter/AreaCenter";
+import Sidebar from "../../../Layout/Sidebar";
 export default function Home() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // lay cai mang userSlide
+  const ListUsers = useSelector((state: any) => state.Users.UserSlice);
+  useEffect(() => {
+    dispatch<any>(getAllUser());
+  }, []);
 
-    // lay cai mang userSlide
-    const ListUsers = useSelector((state: any) => state.Users.UserSlice)
-    useEffect(() => {
-        dispatch<any>(getAllUser())
-    }, [])
+  return (
+    <div className="ContentHomeChat">
+      {/* này là thanh tabbar màu xanh bên trái giống zalo */}
+      <div className="areaLeft bg-blue-500">1</div>
+      {/* <Sidebar /> */}
 
-    return (
-        <div className='ContentHomeChat'>
-            {/* này là thanh tabbar màu xanh bên trái giống zalo */}
-            <div className='areaLeft bg-blue-500'>
-                1
+      {/* This is the user display section */}
+      <div className="areaCenter">
+        <AreaCenter listUser={ListUsers} />
+      </div>
 
-            </div>
-
-
-
-
-            {/* này là phần hiển thị user */}
-            <div className='areaCenter'>
-
-                
-                <AreaCenter
-                    listUser={ListUsers}
-                />
-
-                
-            </div>
-
-
-
-
-            {/* này là phần hiển thị tin nhắn */}
-
-            <div className='areaRight bg-gray-400'>
-                3
-            </div>
-
-
-        </div>
-    )
+      {/* This is the message display section */}
+      <div className="areaRight bg-gray-400 w-full md:w-1/4">3</div>
+    </div>
+  );
 }
